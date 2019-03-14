871bb4a3737a0609e0ecddeef44de74a4c62613d
Selin Acar <selin@selins-mac.local>
2019-03-14 00:09:01 -0400

    Initial Commit

---
+++ 2019-03-14-Assignment2-Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2019-03-14-Assignment2-Set
+//
+//  Created by Selin Denise Acar on 2019-03-14.
+//  Copyright © 2019 Selin Denise Acar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
